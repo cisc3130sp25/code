ca9188ca7d4bd311246c3f18d86f9efb5239d1af
moshe <moshe.lach@yahoo.com>
2025-03-02 17:03:08 -0500

    updates

--- src/F_search_sort/GenericSelectionSort.java
+++ src/F_search_sort/GenericSelectionSort.java
@@ -1,8 +1,25 @@
 package F_search_sort;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class GenericSelectionSort {
+    public static void main(String[] args) {
+        String[] a = {"cc", "aaa", "z"};
+
+        selectionSort(a);
+        System.out.println(Arrays.toString(a)); // [aaa, cc, z]
+
+        Comparator<String> byLength = new Comparator<>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return Integer.compare(s1.length(), s2.length());
+            }
+        };
+        selectionSort(a, byLength);
+        System.out.println(Arrays.toString(a)); // [z, cc, aaa]
+    }
+
     public static <E extends Comparable<E>> void selectionSort(E[] a) {
         for (int i = 0; i < a.length - 1; i++) {
             int indexOfMin = i;
@@ -23,9 +40,7 @@ public class GenericSelectionSort {
         list[j] = temp;
     }
 
-    // for example, if E = Integer, we can use a Comparator<Integer>,
-    // or a Comparator<Number>, or a Comparator<Object>
-    public static <E> void selectionSort(E[] a, Comparator<? super E> comp) {
+    public static <E> void selectionSort(E[] a, Comparator<E> comp) {
         for (int i = 0; i < a.length - 1; i++) {
             int indexOfMin = i;
 
