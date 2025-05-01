b3422293f08d3c30e3f56da6839936fef4efdd9d
moshe <moshe.lach@yahoo.com>
2025-05-01 07:52:33 -0400

    lambdas

---
+++ src/O_lambdas/Name.java
@@ -0,0 +1,4 @@
+package O_lambdas;
+
+public record Name(String first, String last) {
+}
---
+++ src/O_lambdas/StringConsumer.java
@@ -0,0 +1,6 @@
+package O_lambdas;
+
+@FunctionalInterface
+public interface StringConsumer {
+    void accept(String s); // functional method
+}
